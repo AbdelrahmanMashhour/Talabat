7a9676eef8cc29119fa1a9b01e98a1ca4a0078bc
Abdelrahman Mashhour <abdofamous458@gmail.com>
2026-03-23 15:24:30 +0200

    clean program.cs file

---
+++ Talabat.API/Extensions/ApplicationServicesExtension.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Talabat.API.DTOs.Helpers;
+using Talabat.API.Errors;
+using Talabat.API.Middlewares;
+using Talabat.Core.Repositories.Contracts;
+using Talabat.Repository;
+
+namespace Talabat.API.Extensions
+{
+    public static class ApplicationServicesExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<ExceptionMiddleware>();
+            services.AddAutoMapper(typeof(MappingProfile));
+            //services.AddAutoMapper(m=>m.AddProfile(new MappingProfile));
+            #region Configuration Of BadRequest Response (this if found validation request error)
+            services.Configure<ApiBehaviorOptions>(option =>
+            {
+                option.InvalidModelStateResponseFactory = (ActionContext context) =>
+                {
+                    var errors = context.ModelState.Where(e => e.Value.Errors.Count > 0)
+                        .SelectMany(x => x.Value.Errors)
+                        .Select(x => x.ErrorMessage).ToArray();
+                    var errorResponse = new ApiValidationErrorResponse
+                    {
+                        Errors = errors
+                    };
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
+            #endregion
+
+            return services;
+        }
+    }
+}
---
+++ Talabat.API/Extensions/SwaggerServicesExtension.cs
@@ -0,0 +1,22 @@
+﻿namespace Talabat.API.Extensions
+{
+    public static class SwaggerServicesExtension
+    {
+        public static IServiceCollection AddSwaggerServices(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            return services;
+        }
+
+        public static WebApplication UseSwaggerServices(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            return app;
+        }
+    }
+}
